bcab7085240d0496b33c4561471c779012ac795f
Sarah <sarahlchavez@gmail.com>
2021-11-12 10:39:04 -0800

    commit tree 6ca1b147cc34724202c4224379d5b554f3f58721

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,46 @@
 import React from "react";
 
+// takes in one prop: handleLogin which is a function
+// this function is called when the form is submitted
 class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+      
+      //username: e.target.value,
+      //password: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(!this.state.username || !this.state.password) return 
+    this.props.handleLogin(this.state)
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit} >
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleChange} value={this.state.username} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleChange} value={this.state.password}  />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,32 @@
 import React from "react";
 
+// a controlled form derives its input values from state
+// this component takes one prop: maxChars, which is a number-the max characters a msg can have
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }  
+  
+  handleChange = (e) => {
+    this.setState({ 
+      message: e.target.value
+    })
   }
-
+  
+  // make this a controlled component by adding the attributes to the <input> element
+  // its value should be saved in the component's state and should update on every change to the input
+  // show the remaining characters in the component.
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={this.handleChange}/>
+        <h4>Characters Left: {this.props.maxChars - this.state.message.length} </h4>
+
       </div>
     );
   }
